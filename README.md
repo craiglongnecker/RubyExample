5b74096b731bbc89d3d22f98f7428d603507d01a
Craig Longnecker <craig2020@q.com>
2019-03-14 15:53:17 -0500

    Finalized comments

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# RubyExample
+# Ruby Example
+
+
+This program is an example of a console program in Ruby done in Eclipse.
+The program creates and modifies a To Do list of tasks.
+
+The program uses modules, classes, public and private methods, accessor
+methods, if, case and until expressions, constants, exceptions, mixins, objects,
+enumerables, local and instance variables, an array, strings, user input,
+output, adds tasks, shows tasks, updates tasks, deletes tasks, saves tasks
+to a text file, opens an existing text file with tasks and adds the tasks
+to the To Do list, and allows the user to toggle each task to show whether
+the task is completed or not completed.
--- todo.rb
+++ todo.rb
@@ -1,8 +1,9 @@
 # An example of a ruby console project
+# Craig Longnecker
 # Menu Module
 module Menu
   
-  def menu # Menu method for To Do List to display options
+  def menu # Menu method for To Do List to display options that are available for the user
     'Here are the Menu Options for your To Do List:
     1 - Add Task to the To Do List
     2 - Show all of the Tasks currently in the To Do List
@@ -12,56 +13,58 @@ module Menu
     6 - Read a Previous To Do List File and add to the current List
     7 - Toggle Status whether Task is completed or not
     Q - Quit the Program'
-  end
+  end # End of menu method
   
   def show # Show method
     menu # Show menu method above
-  end
+  end # End of show method
   
-end
+end # End of Menu module
 
 #Promptable module
 module Promptable
   
-  def prompt(message = 'What would you like to do?', symbol = ':> ') # Prompt method to prompt the user and accept user input
-     print message
+  def prompt(message = 'What would you like to do?', symbol = ':> ') # Prompt method to prompt the user with a message and accept user input
+     print message # Default message and symbol are provided if the prompt method variables are not overridden
      print symbol
      gets.chomp
-  end
+  end # End of prompt method
   
-end
+end # End of Promptable module
 
 # List Class
 class List
   attr_reader :all_tasks # Attribute reader method that stores all tasks
   
+  # Public methods follow
+  
   def initialize # Initialize method that is similar to a Constructor in Java
     @all_tasks = [] # Method includes an array that stores all tasks
-  end
+  end # End of initialize method
   
   def add(task) # Add task method
     unless task.to_s.chomp.empty? # Checks to see if array is empty
       all_tasks << task # Adds/Pushes a task to the all tasks array
-    end
-  end
+    end # End of unless expression
+  end # End of add method.
   
   def show # Show tasks method
     all_tasks.map.with_index { |l, i| "(#{i.next}): #{l}"} # Show all tasks array in numerical order and whether a task is completed
-  end
+  end # End of show method
   
   def delete(task_number)
     all_tasks.delete_at(task_number  - 1) # Deletes Task number selected
     puts "Task (#{task_number}) has been deleted" # Confirms that Task has been deleted
-  end
+  end # End of delete method
   
   def update(task_number, task) # Update task based on task number
     all_tasks[task_number -1] = task # Updates task
-  end
+  end # End of update method
   
   def write_to_file(filename) # Take name and status of file, output and save it to a new file
     completed =  @all_tasks.map(&:to_machine).join("\n")
     IO.write(filename, completed)
-  end
+  end # End of write_to_file method
   
   def read_from_file(filename) # Input existing file
     IO.readlines(filename).each do |line|
@@ -69,82 +72,84 @@ class List
       status = status.upcase.include?('X')
       add(Task.new(description.join(':').strip, status))
       puts line
-    end
-  end
+    end # End of input, output
+  end # End of read_from_files method
   
   def toggle(task_number) # Toggle method that calls the toggle_status method on the appropriate task from the all_tasks array
     all_tasks[task_number - 1].toggle_status
-  end 
+  end # End of toggle method
   
-end
+end # End of List Class
 
 # Task Class
 class Task
   attr_reader :description # Attribute reader method for description
   attr_accessor :completed_status # Attribute accessor method for status
+  
+  # Public methods follow
     
   def initialize(description, completed_status = false) # Initialize method that is similar to a Constructor in Java
     @description = description # Method initializes each task object with a description
-    @completed_status = completed_status # Boolean value to determine whether task is complete or not complete
-  end
+    @completed_status = completed_status # Boolean value to determine whether task is complete or not complete, default is false
+  end # End of initialize method
   
   def completed? # Is task completed? method
     completed_status # True or false
-  end
+  end # End of completed? method
   
   def to_machine # To_machine method to display completion status and description of Task
     "#{represent_status} : #{description}"
-  end
+  end # End of to_machine method
   
   def toggle_status # Toggle status method that negates the completed? method above
     @completed_status = !completed?
-  end
+  end # End of toggle_status method
   
   def to_s # Method to display completion status and description of Task
     "#{represent_status} : #{description}"
-  end
+  end # End of to_s method
   
   private # Private methods follow
   
   def represent_status # Private represent status method to determine if task is completed or not
     "#{completed? ? '[X]' : '[ ]'}" # Boolean with [X] is complete, [ ] is not complete
-  end
+  end # End of private represent status method
 
-end
+end # End of Task Class
 
 
 # Run Program
-if __FILE__ == $PROGRAM_NAME # Create new list file when program is run
+if __FILE__ == $PROGRAM_NAME # If expression to create new List file when program is run
   include Menu # Include Menu module
   include Promptable # Include Promptable module
   todo_list = List.new # Create new List
   puts 'Please choose from the following list:'
-    until ['Q'].include?(user_input = prompt(show).upcase) # Until statement to continue to run program until user enters 'Q' or 'q' to quit program
-      case user_input # Begin case statements from user input
-        when '1' # Case statement when '1' (add task to List) is entered
+    until ['Q'].include?(user_input = prompt(show).upcase) # Until expression to continue to run program until user enters 'Q' or 'q' to quit program
+      case user_input # Begin case expression from user input
+        when '1' # Case expression when '1' (add task to List) is entered
           todo_list.add(Task.new(prompt('What is the Task description you would like to add to your List?')))          
-        when '2' # Case statement when '2' (show all tasks in List) is entered
+        when '2' # Case expression when '2' (show all tasks in List) is entered
           puts todo_list.show
-        when '3' # Case statement when '3' (update task from List) is entered
+        when '3' # Case expression when '3' (update task from List) is entered
           todo_list.update(prompt('What is the number of the Task in the List you wish to update?').to_i,
-            Task.new(prompt('What is the new Task description?')))     
-        when '4' # Case statement when '4' (delete Task from List) is entered
-          todo_list.delete(prompt('What is the number of the Task in the List you wish to delete?').to_i)
-        when '5' # Case statement when '5' (save List to text file) is entered
-          todo_list.write_to_file(prompt('What is the name of the file for the List you wish to save?'))
-        when '6' # Case statement when '6' (open existing List text file) is entered
-          begin # Read file if correct file name is provided
-            todo_list.read_from_file(prompt('What is the name of the List file you wish to view and add to List?'))
-          rescue Errno::ENOENT # Error statement if incorrect file name is provided
+            Task.new(prompt('What is the new Task description?'))) # Modified prompt methods
+        when '4' # Case expression when '4' (delete Task from List) is entered
+          todo_list.delete(prompt('What is the number of the Task in the List you wish to delete?').to_i) # Modified prompt method
+        when '5' # Case expression when '5' (save List to text file) is entered
+          todo_list.write_to_file(prompt('What is the name of the file for the List you wish to save?')) # Modified prompt method
+        when '6' # Case expression when '6' (open existing List text file) is entered
+          begin # Begin exception to read file if correct file name is provided
+            todo_list.read_from_file(prompt('What is the name of the List file you wish to view and add to List?')) # Modified prompt method
+          rescue Errno::ENOENT # Error exception that displays error statement if incorrect file name is provided
             puts('File name not found.  Please verify your file name and path')
           end
-        when '7' # Case statement when '7' (to toggle between completed or not completed Task) is entered
-          puts todo_list.show
-          todo_list.toggle(prompt('Which Task number would you like to toggle between [X] currently complete and [ ] for currently not complete?').to_i)         
-      else # Else statement to catch invalid input
+        when '7' # Case expression when '7' (to toggle between completed or not completed Task) is entered
+          puts todo_list.show # Show List of tasks
+          todo_list.toggle(prompt('Which Task number would you like to toggle between [X] currently complete and [ ] for currently not complete?').to_i) # Modified prompt method
+      else # Else expression to catch invalid input
           puts 'Sorry, your input was not valid.'
-      end
-      prompt('Press enter to continue', '') # Prompt method
-    end
+      end # End of case expression
+      prompt('Press enter to continue', '') # Modified prompt method
+    end # End of until expression
   puts 'Goodbye! - Thanks for using this To Do List program!' # End of program
-end
+end # End of if expression
